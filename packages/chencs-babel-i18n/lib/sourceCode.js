3b30884ffddf77abcfa0f286b8f5787bf351b69e
chencs-19950208 <71002903+chencs-19950208@users.noreply.github.com>
2022-09-16 17:52:14 +0800

    feat: 国际化插件 (#3)
    Co-authored-by: 陈春生 <chen.chunsheng@coolcollege.cn>

---
+++ packages/chencs-babel-i18n/lib/sourceCode.js
@@ -0,0 +1,22 @@
+/**
+ * App 示例代码
+ */
+function App() {
+    const title = 'title';
+    const desc = `desc`;
+    const desc2 = /*i18n-disable*/`desc`;
+    const desc3 = `aaa ${ title + desc} bbb ${ desc2 } ccc`;
+
+    return (
+      <div className="app" title={"测试"}>
+        <img src={Logo} />
+        <h1>${title}</h1>
+        <p>${desc}</p>  
+        <div>
+        {
+          '中文'
+        }
+        </div>
+      </div>
+    );
+  }
