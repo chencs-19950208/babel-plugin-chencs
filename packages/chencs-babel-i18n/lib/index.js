d31ff81497ef9e1becd9cbc06dd923934ad6d656
陈春生 <chen.chunsheng@coolcollege.cn>
2022-09-16 17:43:48 +0800

    feat: 国际化插件

--- packages/chencs-babel-i18n/lib/index.js
+++ packages/chencs-babel-i18n/lib/index.js
@@ -1,7 +1,30 @@
-'use strict';
+const { transformFromAstSync } = require('@babel/core');
+const parser = require('@babel/parser');
+const chencsAutoPlugini18n = require('./chencs-i18n');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = chencsBabelI18n;
+// 读到源码字符串
+const sourceCode = fs.readFileSync(path.join(__dirname, './sourceCode.js'), {
+  encoding: 'utf-8',
+});
 
-function chencsBabelI18n() {
-    // TODO
+// 源代码转AST
+const AST_CODE = parser.parse(sourceCode, {
+  sourceType: 'unambiguous',
+  plugins: ['jsx']
+});
+
+// 新的源码
+const { code } = transformFromAstSync(AST_CODE, {
+  plugins: [[chencsAutoPlugini18n, {
+    outputDir: path.resolve(__dirname, './output')
+  }]]
+});
+
+// 写入到新的文件中
+if (code) {
+  console.log(code);
+  fs.writeFileSync('./newCode.js', code);
 }
+
